37f10a1766b91e24ea0dd4d78d8aad4985021196
Fiona <36171694+fkd5@users.noreply.github.com>
2018-04-26 11:53:00 -0400

    more outline

--- joint.py
+++ joint.py
@@ -4,23 +4,36 @@ class Joint:
 	A joint.
 	'''
 
-	def __init__(self, ID, loc, prev=Null, nex=Null, weight=1, mloc=0, mdir=0):
+	def __init__(self, ID, loc, pred=Null, suc=Null, weight=1, mloc=0, mdir=0):
 		'''
 		init
 		'''
 
+		LENGTH = 10
+
 		IDnumber = ID
 		location = loc
-		prev = prev
-		nex = nex
+		pred = pred
+		suc = suc
 		weight = weight
 		mlocation = mloc
 		mdirection = mdir
+
+
+	def changesuc(self, joint):
+		self.suc = joint
+
+
+	def changepred(self, joint):
+		self.pred = joint
+
 
 	def addmomentum(self, mloc, mdir):
 		'''
 		Add current momentum and new momentum as vectors.
 		'''
 
-	
+	def findnewlocfromangle()
+
+
 
--- run.py
+++ run.py
@@ -2,44 +2,80 @@
 
 
 if __name__ == "__main__":
-	joints = getjointsfromuser()
 	printbackground()
-	printjoints(joints)
+	joint = getjointsfromuser()
+	printjoints(joint)
 	ITERATIONS = 20
-	getinitialmomentum(joints)
+	getinitialmomentum(joint)
 	for i in range(0, ITERATIONS):
-		applymomentum(joints)
-		printjoints(joints)
+		applymomentum(joint)
+		printjoints(joint)
 		#wait 2 seconds
 
 
 
 def getjointsfromuser():
 	'''
-	Gets list of joints by user input
+	Gets list of joints by user input.
+
+	INPUT: none
+
+	RETURN:
+		firstjoint: *joint*
+			First joint on linked list of joints.
 	'''
+
+	#prevjoint = Null
+	#FIRST one not part of while loop
+	# get loc and weight
+	# set to prevjoint
+	#while loop for while user still wants to input joints (input isn't 'stop' or more than 10 joints)
+		#get joint location by asking for angle and user some function somewhere
+			#prevjoint.findlocfromangle()
+		#get joint weight
+		# attach to prevjoint
+		# printjoints(firstjoint)
+	#ask user if they want to attach the first and last joint to make a circle thing
+
+
+	return firstjoint
+
+
+def getjointsfromfile():
+	'''
+	Gets list of joints from file input
+	'''
+	pass
 
 
 def printbackground():
 	'''
-	Print background
+	Print background. Have cool looking blue or red background with time counter in corner and maybe a
+	frame thing or a pattern in the background.
+	'''
+
+	pass
+
+
+def printjoints(joint):
+	'''
+	Print joints to file with links and ID numbers.  Joints in black to white depending on weight.
+	Links in between joints.  ID numbers next to joints (or inside the joints in strong color).
+	
+
+	basically just use imagedraw module from PIL for everything.
+	https://pillow.readthedocs.io/en/3.1.x/reference/ImageDraw.html
 	'''
 
 
-def printjoints(joints):
+def getinitialmomentum(joint):
 	'''
-	Print joints to file with links and ID numbers
-	'''
-
-
-def getinitialmomentum(joints):
-	'''
-	Get momentum from user.
+	Get momentum from user.  Also ask which joint to apply momentum to.
 	Assign momentum to correct joint.
 	'''
 
 
-def assignmomentum(joints):
+def applymomentum(joint):
 	'''
 	DO STUFF
 	'''
